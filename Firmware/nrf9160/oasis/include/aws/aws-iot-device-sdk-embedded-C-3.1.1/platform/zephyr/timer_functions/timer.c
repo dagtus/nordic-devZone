4e92f663e5b154850dab18dbfa966b6127b235b0
cortexr52 <70549542+cortexr52@users.noreply.github.com>
2020-11-19 13:16:09 -0700

    Update timer.c
    Adding second options in int_timer,

--- Firmware/nrf9160/oasis/include/aws/aws-iot-device-sdk-embedded-C-3.1.1/platform/zephyr/timer_functions/timer.c
+++ Firmware/nrf9160/oasis/include/aws/aws-iot-device-sdk-embedded-C-3.1.1/platform/zephyr/timer_functions/timer.c
@@ -111,7 +111,10 @@ uint32_t left_ms(Timer *timer)
 
 void init_timer(Timer *timer)
 {
-	k_timer_init(&timer->end_time, NULL, NULL);
+	//Both options was used, we do need expiry_dummy_fn & stop_dummy_fn, just added
+	// to follow the docummentation.
+	k_timer_init(&timer->end_time, expiry_dummy_fn, stop_dummy_fn);
+	//k_timer_init(&timer->end_time, NULL, NULL);
 }
 
 /**
